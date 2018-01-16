08f6c47d0b28c70704a85a6a93503a56830cfc5b
LOHIKA\mkryvoplias <mkryvoplias@lohika.com>
2018-01-16 13:56:04 +0200

    update script for installation dotnet

--- dotnet-linux-install.sh
+++ dotnet-linux-install.sh
@@ -1,6 +1,6 @@
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
 sudo apt-get update
 
-sudo apt-get --assume-yes install dotnet-sdk-2.1.4
+sudo apt-get --assume-yes --allow-unauthenticated install dotnet-sdk-2.1.4
 
 dotnet --version
