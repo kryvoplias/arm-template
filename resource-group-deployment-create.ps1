c686f5775395e8332218e37cc69bf559b10c42b8
Maksym Kryvoplias <m.kryvoplias@gmail.com>
2018-01-21 23:12:00 +0200

    provide random update tag

--- resource-group-deployment-create.ps1
+++ resource-group-deployment-create.ps1
@@ -1,1 +1,5 @@
-az group deployment create --resource-group mksm-resource-group --template-uri "https://raw.githubusercontent.com/kryvoplias/arm-template/master/template.json"
+$UTCNow = (Get-Date).ToUniversalTime()
+
+$UTCTimeTick = $UTCNow.Ticks.tostring()
+
+az group deployment create --resource-group mksm-resource-group --template-uri "https://raw.githubusercontent.com/kryvoplias/arm-template/master/template.json" --parameters currentDateTimeInTicks=$UTCTimeTick
