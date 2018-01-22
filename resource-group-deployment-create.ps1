cc76c91cda20c0c38085ec342b458aaef8f9ba2d
LOHIKA\mkryvoplias <mkryvoplias@lohika.com>
2018-01-22 18:27:10 +0200

    testing lb outputs

--- resource-group-deployment-create.ps1
+++ resource-group-deployment-create.ps1
@@ -3,3 +3,5 @@ $UTCNow = (Get-Date).ToUniversalTime()
 $UTCTimeTick = $UTCNow.Ticks.tostring()
 
 az group deployment create --resource-group mksm-resource-group --template-uri "https://raw.githubusercontent.com/kryvoplias/arm-template/master/template.json" --parameters currentDateTimeInTicks=$UTCTimeTick
+
+az group deployment create --resource-group mksm-resource-group --template-uri "https://raw.githubusercontent.com/kryvoplias/arm-template/master/template-lb.json"
